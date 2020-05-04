ba2d459f5ddaeb78dd8b0be31be71d2be5b9ab6d
cooljasonmelton <cooljasonmelton>
2020-05-04 11:47:48 -0500

    set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      what up
+
     </div>
   );
 }
 
 export default App;
+
+
+// color ideas Hex: 9F4298 / CFDB31 / D1AFD3 / E6E7E8
+
